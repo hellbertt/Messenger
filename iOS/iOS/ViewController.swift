a4a1e30707a4c6a059f40dc0e9e111d7d17e7b75
Mariana Rios Silveira Carvalho <marianariossc@gmail.com>
2018-09-11 14:18:06 -0300

    Create xcode project

---
+++ iOS/iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Mariana Rios Silveira Carvalho on 11/09/18.
+//  Copyright © 2018 Helbert Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
